2bb54cb98b4f9156f53344ab053c12ff6dd6868c
crccheck <c@crccheck.com>
2012-07-26 11:58:35 -0500

    add basic ipython new style configuration file

---
+++ .ipython/ipython_config.py
@@ -0,0 +1,7 @@
+# see https://github.com/jni/ipython-config/blob/master/ipython_config.py
+
+c = get_config()
+
+c.TerminalInteractiveShell.autocall = 1
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.colors = 'Linux'
