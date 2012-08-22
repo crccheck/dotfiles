070e0fc94a55d20a42427f388440a749d1794fae
crccheck <c@crccheck.com>
2012-08-22 13:34:43 -0500

    sb2 update

--- sublime-text-2/Packages/CSS/css_completions.py
+++ sublime-text-2/Packages/CSS/css_completions.py
@@ -1,42 +1,50 @@
 import sublime, sublime_plugin
 import re
 
+
+common = {  "color": ["rgb($1)", "rgba($1)", "hsl($1)", "hsla($1)", "transparent"],
+            "uri": ["url($1)"],
+            "border-style": ["none", "hidden", "dotted", "dashed", "solid", "double", "groove", "ridge", "inset", "outset"],
+            "border-width": ["thin", "medium", "thick"],
+            "shape": ["rect($1)"],
+            "generic-family": ["serif", "sans-serif", "cursive", "fantasy", "monospace"] }
+
 css_data = """
 "background-attachment"=scroll | fixed | inherit
 "background-color"=<color> | inherit
 "background-image"=<uri> | none | inherit
-"background-position"=[ [ <percentage> | <length> | left | center | right ] [ <percentage> | <length> | top | center | bottom ]? ] | [ [ left | center | right ] || [ top | center | bottom ] ] | inherit 0% 0%
+"background-position"=left | center | right | top | bottom | inherit
 "background-repeat"=repeat | repeat-x | repeat-y | no-repeat | inherit
-"background"=['background-color' || 'background-image' || 'background-repeat' || 'background-attachment' || background-position] | inherit
+"background"=<color> | <uri> | repeat | repeat-x | repeat-y | no-repeat | scroll | fixed | left | center | right | top | bottom | inherit
 "border-collapse"=collapse | separate | inherit
-"border-color"=[ <color> ]{1,4} | inherit
-"border-spacing"=<length> <length>? | inherit
-"border-style"=<border-style>{1,4} | inherit
-"border-top" "border-right" "border-bottom" "border-left"=[ <border-width> || <border-style> || border-top-color ] | inherit
+"border-color"=<color> | inherit
+"border-spacing"=inherit
+"border-style"=<border-style> | inherit
+"border-top" "border-right" "border-bottom" "border-left"=<border-width> | <border-style> | <color> | inherit
 "border-top-color" "border-right-color" "border-bottom-color" "border-left-color"=<color> | inherit
 "border-top-style" "border-right-style" "border-bottom-style" "border-left-style"=<border-style> | inherit
 "border-top-width" "border-right-width" "border-bottom-width" "border-left-width"=<border-width> | inherit
-"border-width"=<border-width>{1,4} | inherit
-"border"=[ <border-width> || <border-style> || border-top-color ] | inherit
+"border-width"=<border-width> | inherit
+"border"= <border-width> | <border-style> | <color> | inherit
 "bottom"=<length> | <percentage> | auto | inherit
 "caption-side"=top | bottom | inherit
 "clear"=none | left | right | both | inherit
 "clip"=<shape> | auto | inherit
 "color"=<color> | inherit
-"content"=normal | none | [ <string> | <uri> | <counter> | attr(<identifier>) | open-quote | close-quote | no-open-quote | no-close-quote ]+ | inherit
-"counter-increment"=[ <identifier> <integer>? ]+ | none | inherit
-"counter-reset"=[ <identifier> <integer>? ]+ | none | inherit
-"cursor"=[ [<uri> ,]* [ auto | crosshair | default | pointer | move | e-resize | ne-resize | nw-resize | n-resize | se-resize | sw-resize | s-resize | w-resize | text | wait | help | progress ] ] | inherit
-"direction"=ltr | rtl | inherit ltr all elements, but see prose yes
+"content"=normal | none | <uri> | open-quote | close-quote | no-open-quote | no-close-quote | inherit
+"counter-increment"=none | inherit
+"counter-reset"=none | inherit
+"cursor"=<uri> | auto | crosshair | default | pointer | move | e-resize | ne-resize | nw-resize | n-resize | se-resize | sw-resize | s-resize | w-resize | text | wait | help | progress | inherit
+"direction"=ltr | rtl | inherit
 "display"=inline | block | list-item | inline-block | table | inline-table | table-row-group | table-header-group | table-footer-group | table-row | table-column-group | table-column | table-cell | table-caption | none | inherit
 "empty-cells"=show | hide | inherit
 "float"=left | right | none | inherit
-"font-family"=[ [ <family-name> | <generic-family> ] [, <family-name>| <generic-family> ]* ] | inherit
-"font-size"=<absolute-size> | <relative-size> | <length> | <percentage> | inherit
+"font-family"=<generic-family>| inherit
+"font-size"=inherit
 "font-style"=normal | italic | oblique | inherit
 "font-variant"=normal | small-caps | inherit
 "font-weight"=normal | bold | bolder | lighter | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | inherit
-"font"=[ [ 'font-style' || 'font-variant' || 'font-weight' ]? 'font-size' [ / 'line-height' ]? font-family ] | caption | icon | menu | message-box | small-caption | status-bar | inherit
+"font"=normal | italic | oblique | normal | small-caps | normal | bold | bolder | lighter | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | normal | <generic-family> | caption | icon | menu | message-box | small-caption | status-bar | inherit
 "height"=<length> | <percentage> | auto | inherit
 "left"=<length> | <percentage> | auto | inherit
 "letter-spacing"=normal | <length> | inherit
@@ -44,72 +52,66 @@ css_data = """
 "list-style-image"=<uri> | none | inherit
 "list-style-position"=inside | outside | inherit
 "list-style-type"=disc | circle | square | decimal | decimal-leading-zero | lower-roman | upper-roman | lower-greek | lower-latin | upper-latin | armenian | georgian | lower-alpha | upper-alpha | none | inherit
-"list-style"=[ 'list-style-type' || 'list-style-position' || list-style-image ] | inherit
+"list-style"=disc | circle | square | decimal | decimal-leading-zero | lower-roman | upper-roman | lower-greek | lower-latin | upper-latin | armenian | georgian | lower-alpha | upper-alpha | none | inside | outside | <uri> | inherit
 "margin-right" "margin-left"=<margin-width> | inherit
 "margin-top" "margin-bottom"=<margin-width> | inherit
-"margin"=<margin-width>{1,4} | inherit
+"margin"=<margin-width> | inherit
 "max-height"=<length> | <percentage> | none | inherit
 "max-width"=<length> | <percentage> | none | inherit
 "min-height"=<length> | <percentage> | inherit
 "min-width"=<length> | <percentage> | inherit
 "opacity"=<number> | inherit
-"orphans"=<integer> | inherit 2
+"orphans"=<integer> | inherit
 "outline-color"=<color> | invert | inherit
 "outline-style"=<border-style> | inherit
 "outline-width"=<border-width> | inherit
-"outline"=[ 'outline-color' || 'outline-style' || outline-width ] | inherit
+"outline"=<color> | <border-style> | <border-width> | inherit
 "overflow"=visible | hidden | scroll | auto | inherit
 "padding-top" "padding-right" "padding-bottom" "padding-left"=<padding-width> | inherit
-"padding"=<padding-width>{1,4} | inherit
+"padding"=<padding-width> | inherit
 "page-break-after"=auto | always | avoid | left | right | inherit
 "page-break-before"=auto | always | avoid | left | right | inherit
 "page-break-inside"=avoid | auto | inherit
 "position"=static | relative | absolute | fixed | inherit
-"quotes"=[<string> <string>]+ | none | inherit
+"quotes"=none | inherit
 "right"=<length> | <percentage> | auto | inherit
 "table-layout"=auto | fixed | inherit
 "text-align"=left | right | center | justify | inherit
-"text-decoration"=none | [ underline || overline || line-through || blink ] | inherit none
+"text-decoration"=none | underline | overline | line-through | blink | inherit | none
 "text-indent"=<length> | <percentage> | inherit
-"text-transform"=capitalize | uppercase | lowercase | none | inherit none
+"text-transform"=capitalize | uppercase | lowercase | none | inherit
 "top"=<length> | <percentage> | auto | inherit
 "unicode-bidi"=normal | embed | bidi-override | inherit
-"vertical-align"=baseline | sub | super | top | text-top | middle | bottom | text-bottom | <percentage> | <length> | inherit baseline
+"vertical-align"=baseline | sub | super | top | text-top | middle | bottom | text-bottom | <percentage> | <length> | inherit
 "visibility"=visible | hidden | collapse | inherit
 "white-space"=normal | pre | nowrap | pre-wrap | pre-line | inherit
-"widows"=<integer> | inherit 2
+"widows"=<integer> | inherit
 "width"=<length> | <percentage> | auto | inherit
 "word-spacing"=normal | <length> | inherit
 "z-index"=auto | <integer> | inherit
 
 
-"background"=[ <bg-layer> , ]* <final-bg-layer>
-"background-attachment"=<attachment> [ , <attachment> ]*
-"background-clip"=<box> [ , <box> ]*
-"background-color"=<color>
-"background-image"=<bg-image> [ , <bg-image> ]*
-"background-origin"=<box> [ , <box> ]*
-"background-position"=<position> [ , <position> ]*
-"background-repeat"=<repeat-style> [ , <repeat-style> ]*
-"background-size"=<bg-size> [ , <bg-size> ]*
-"border"=<border-width> || <border-style> || <color>
-"border-color"=<color>{1,4}
-"border-image"=<'border-image-source'> || <'border-image-slice'> [ / <'border-image-width'> | / <'border-image-width'>? / <'border-image-outset'> ]? || <'border-image-repeat'>
-"border-image-outset"=[ <length> | <number> ]{1,4}
-"border-image-repeat"=[ stretch | repeat | round | space ]{1,2}
-"border-image-slice"=[<number> | <percentage>]{1,4} && fill?
+"background-clip"=<box>
+"background-origin"=<box>
+"background-size"=<bg-size>
+"border"=<border-width> | <border-style> | <color>
+"border-color"=<color>
+"border-image"=<border-image-source> | <border-image-slice> | <border-image-width> | <border-image-width> | <border-image-outset> | <border-image-repeat>
+"border-image-outset"=<length> | <number>
+"border-image-repeat"=stretch | repeat | round | space
+"border-image-slice"=<number> | <percentage>
 "border-image-source"=none | <image>
-"border-image-width"=[ <length> | <percentage> | <number> | auto ]{1,4}
-"border-radius"=[ <length> | <percentage> ]{1,4} [ / [ <length> | <percentage> ]{1,4} ]?
-"border-style"=<border-style>{1,4}
-"border-top" "border-right" "border-bottom" "border-left"=<border-width> || <border-style> || <color>
+"border-image-width"=<length> | <percentage> | <number> | auto
+"border-radius"=<length> | <percentage>
+"border-style"=<border-style>
+"border-top" "border-right" "border-bottom" "border-left"=<border-width> | <border-style> | <color>
 "border-top-color" "border-right-color" "border-bottom-color" "border-left-color"=<color>
-"border-top-left-radius" "border-top-right-radius" "border-bottom-right-radius" "border-bottom-left-radius"=[ <length> | <percentage> ]{1,2}
+"border-top-left-radius" "border-top-right-radius" "border-bottom-right-radius" "border-bottom-left-radius"=<length> | <percentage>
 "border-top-style" "border-right-style" "border-bottom-style" "border-left-style"=<border-style>
 "border-top-width" "border-right-width" "border-bottom-width" "border-left-width"=<border-width>
-"border-width"=<border-width>{1,4}
+"border-width"=<border-width>
 "box-decoration-break"=slice | clone
-"box-shadow"=none | <shadow> [ , <shadow> ]* none
+"box-shadow"=none | <shadow> | none
 """
 
 def parse_css_data(data):
@@ -123,9 +125,10 @@ def parse_css_data(data):
         allowed_values = []
         for v in values.split('|'):
             v = v.strip()
-            if v == "" or v[0] == '<' or v[0] == '[':
-                allowed_values = []
-                break
+            if v[0] == '<' and v[-1] == '>':
+                key = v[1:-1]
+                if key in common:
+                    allowed_values += common[key]
             else:
                 allowed_values.append(v)
 
@@ -162,15 +165,21 @@ class CSSCompletions(sublime_plugin.EventListener):
                 if prop_name in self.props:
                     values = self.props[prop_name]
 
-                    add_semi_colon = view.substr(sublime.Region(loc, loc + 1)) != ';'
+                    add_semi_colon = view.substr(sublime.Region(locations[0], locations[0] + 1)) != ';'
 
                     for v in values:
-                        if add_semi_colon:
-                            l.append((v, v + ";"))
-                        else:
-                            l.append((v, v))
+                        desc = v
+                        snippet = v
 
-                    return (l, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+                        if add_semi_colon:
+                            snippet += ";"
+
+                        if snippet.find("$1") != -1:
+                            desc = desc.replace("$1", "")
+
+                        l.append((desc, snippet))
+
+                    return (l, sublime.INHIBIT_WORD_COMPLETIONS)
 
             return None
         else:
@@ -182,4 +191,4 @@ class CSSCompletions(sublime_plugin.EventListener):
                 else:
                     l.append((p, p))
 
-            return (l, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+            return (l, sublime.INHIBIT_WORD_COMPLETIONS)
