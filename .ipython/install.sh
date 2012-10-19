1d70303533155daadd807dca50db080089e6fe03
crccheck <c@crccheck.com>
2012-10-19 11:49:39 -0500

    fix my ipython config installer

--- .ipython/install.sh
+++ .ipython/install.sh
@@ -1,6 +1,17 @@
-if [ ! -z $IPYTHONDIR ]; then
-    SOURCE=$(pwd)
-    mkdir -p $IPYTHONDIR
-    cd $IPYTHONDIR
-    ln -s $SOURCE/ipython_config.py
+#
+# $IPYTHONDIR is typically something like /Users/crc/env/tt/.ipython
+#
+if [ -n $IPYTHONDIR ]; then
+    DST=$IPYTHONDIR/profile_default
+    if [ ! -e $DST/ipython_config.py ]; then
+        echo "Copying ipython config into $DST"
+        SOURCE=$(pwd)
+        mkdir -p $DST
+        cd $DST
+        ln -s $SOURCE/ipython_config.py
+    else
+        echo "ipython_config.py already exists"
+    fi
+else
+    echo "IPYTHONDIR is not set"
 fi
