4b4877d698ea3891abe6dd62fd8ef4a08e78de72
crccheck <c@crccheck.com>
2012-12-19 18:47:45 -0600

    sb2 update

--- sublime-text-2/Packages/CoffeeCompile/coffee_compile.py
+++ sublime-text-2/Packages/CoffeeCompile/coffee_compile.py
@@ -41,6 +41,13 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
             return ('', error_message)
 
     def _execute_command(self, args, text, path=None):
+
+        # This is needed for Windows... not sure why. See:
+        # https://github.com/surjikal/sublime-coffee-compile/issues/13
+        if path and PLATFORM_IS_WINDOWS:
+            os.environ['PATH'] = path
+            path = None
+
         env = {'PATH': path} if path else None
         process = subprocess.Popen(args,
             stdin=subprocess.PIPE,
@@ -121,6 +128,7 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
     def _get_path(self):
         node_path   = self.SETTINGS.get('node_path')
         coffee_path = self._get_coffee_path()
+
         path = os.environ.get('PATH', '').split(':')
 
         if node_path:
