43476b13d34d7ab607728180132dc6ed2b16b974
crccheck <c@crccheck.com>
2013-02-17 16:43:50 +0000

    turn autoreload on in ipython

--- .ipython/ipython_config.py
+++ .ipython/ipython_config.py
@@ -5,3 +5,9 @@ c = get_config()
 c.TerminalInteractiveShell.autocall = 1
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.colors = 'Linux'
+
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+c.InteractiveShellApp.exec_lines.append('print "%autoreload activated"')
+
