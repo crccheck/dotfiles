cbaa52067a79516fb7c83ff0fed651144756e77c
crccheck <c@crccheck.com>
2015-03-06 11:16:50 -0600

    begin an osx bootstrapping guide

---
+++ bootstrap_osx.sh
@@ -0,0 +1,27 @@
+# For OSX 10.10 Yosemite
+
+#
+# apps:
+# * bittorrent sync
+
+brew install python mysql gti ponysay jq macvim bash ack gnu-sed chromedriver
+
+brew install caskroom/cask/brew-cask
+
+# get started
+brew cask install firefox google-chrome slate karabiner virtualbox iterm2 keepassx
+# dev
+brew cask install postgres sublime-text
+# apps
+brew cask install vagrant
+
+# also need to get virtualbox extensions
+
+# Preferences
+# -----------
+#
+# ### Keyboard
+# Full Keyboard Access: All controls (or Control F7)
+#
+# Disable updates
+# Hot corners
