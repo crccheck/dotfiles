c1b5a3fd0afb6f3e4f9d6cbed21abf9a40d8f5c5
crccheck <c@crccheck.com>
2022-02-16 12:38:31 -0600

    add miro window manager

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,13 @@
+local hyper = {"ctrl", "alt", "cmd"}
+
+hs.loadSpoon("MiroWindowsManager")
+
+hs.window.animationDuration = 0.3
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {hyper, "up"},
+  right = {hyper, "right"},
+  down = {hyper, "down"},
+  left = {hyper, "left"},
+  fullscreen = {hyper, "f"},
+  nextscreen = {hyper, "n"}
+})
